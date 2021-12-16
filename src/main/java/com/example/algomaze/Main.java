8fee472216b3c02ee9215fccc48988fce0a3ea3f
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2021-12-16 16:08:12 +0100

    Full comment of every Module

--- src/main/java/com/example/algomaze/Main.java
+++ src/main/java/com/example/algomaze/Main.java
@@ -10,45 +10,45 @@ public class Main {
             int userSelected;
             //Option selection
             do {
-                userSelected = MenuData();
-                switch (userSelected) {
-                    case 1 -> {
+                userSelected = MenuData(); //Menu selection
+                switch (userSelected) { //Switch case for menu selection
+                    case 1 -> { //If user selects 1, run the maze generation algorithm in easy mode
                         System.out.println("Labyrinth easy ♥ selected\nthe game will start :");
-                        int x = args.length >= 1 ? (Integer.parseInt(args[0])) : 5;
+                        int x = args.length >= 1 ? (Integer.parseInt(args[0])) : 5; // Set the x and y size of the maze to 5 by 5
                         int y = args.length == 2 ? (Integer.parseInt(args[1])) : 5;
-                        MazeGenerator maze = new MazeGenerator(x, y);
-                        maze.display();
+                        MazeGenerator maze = new MazeGenerator(x, y); // Create a new maze
+                        maze.display(); // Display the maze
                     }
-                    case 2 -> {
+                    case 2 -> { //If user selects 2, run the maze generation algorithm in medium mode
                         System.out.println("Labyrinth medium ★ selected\nthe game will start :");
-                        int x = args.length >= 1 ? (Integer.parseInt(args[0])) : 10;
+                        int x = args.length >= 1 ? (Integer.parseInt(args[0])) : 10; // Set the x and y size of the maze to 10 by 10
                         int y = args.length == 2 ? (Integer.parseInt(args[1])) : 10;
-                        MazeGenerator maze = new MazeGenerator(x, y);
-                        maze.display();
+                        MazeGenerator maze = new MazeGenerator(x, y); //Create a new maze
+                        maze.display(); //Display the generated maze
                     }
-                    case 3 -> {
+                    case 3 -> { //If user selects 3, run the maze generation algorithm in hard mode
                         System.out.println("Labyrinth hard ☠ selected\nthe game will start :");
-                        int x = args.length >= 1 ? (Integer.parseInt(args[0])) : 20;
+                        int x = args.length >= 1 ? (Integer.parseInt(args[0])) : 20; // Set the x and y size of the maze to 20 by 20
                         int y = args.length == 2 ? (Integer.parseInt(args[1])) : 20;
-                        MazeGenerator maze = new MazeGenerator(x, y);
-                        maze.display();
+                        MazeGenerator maze = new MazeGenerator(x, y); //Create a new maze
+                        maze.display(); //Display the generated maze
                     }
-                    case 4 -> SwingUtilities.invokeLater(() -> {
-                        JFrame f = new JFrame();
-                        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                        f.setTitle("Maze Generator");
-                        f.setResizable(false);
-                        f.add(new MazeGeneratorResolver(24), BorderLayout.CENTER);
-                        f.pack();
-                        f.setLocationRelativeTo(null);
-                        f.setVisible(true);
+                    case 4 -> SwingUtilities.invokeLater(() -> { //If user selects 4, resolve the maze
+                        JFrame f = new JFrame(); //Create a new JFrame
+                        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Close the window
+                        f.setTitle("Maze Generator"); //Set the title of the window
+                        f.setResizable(false); //Disable resizing
+                        f.add(new MazeGeneratorResolver(24), BorderLayout.CENTER); //Add the maze resolver to the frame
+                        f.pack(); //Resize the window to fit the maze
+                        f.setLocationRelativeTo(null); //Centers the window
+                        f.setVisible(true); //Make the window visible
                     });
-                    case 5 -> System.exit(0);
+                    case 5 -> System.exit(0); //If user selects 5, exit the program
                     default -> {
                     }
                 }
             }
-            while (userSelected > 5 );
+            while (userSelected > 5 ); // Loop until user selects 5
     }
 
 }
--- src/main/java/com/example/algomaze/MazeGenerator.java
+++ src/main/java/com/example/algomaze/MazeGenerator.java
@@ -7,19 +7,19 @@ import java.util.Arrays;
 /*
  * recursive backtracking algorithm
  */
-public class MazeGenerator {
+public class MazeGenerator { // initialise the variable for the size of the maze
     private final int x;
     private final int y;
-    private final int[][] maze;
+    private final int[][] maze; // initialise the maze array
 
-    public MazeGenerator(int x, int y) {
+    public MazeGenerator(int x, int y) { // constructor of the maze
         this.x = x;
         this.y = y;
         maze = new int[this.x][this.y];
         generateMaze(0, 0);
     }
 
-    public void display() {
+    public void display() { // display the maze
         for (int i = 0; i < y; i++) {
             // draw the north edge
             for (int j = 0; j < x; j++) {
@@ -39,26 +39,26 @@ public class MazeGenerator {
         System.out.println("+");
     }
 
-    private void generateMaze(int cx, int cy) {
-        DIR[] dirs = DIR.values();
-        Collections.shuffle(Arrays.asList(dirs));
-        for (DIR dir : dirs) {
-            int nx = cx + dir.dx;
-            int ny = cy + dir.dy;
-            if (between(nx, x) && between(ny, y)
-                    && (maze[nx][ny] == 0)) {
-                maze[cx][cy] |= dir.bit;
-                maze[nx][ny] |= dir.opposite.bit;
-                generateMaze(nx, ny);
+    private void generateMaze(int cx, int cy) { // generate the maze using the recursive backtracking algorithm
+        DIR[] dirs = DIR.values(); // initialise the array of directions
+        Collections.shuffle(Arrays.asList(dirs)); // shuffle the directions
+        for (DIR dir : dirs) { // for each direction
+            int nx = cx + dir.dx; // calculate the new x coordinate
+            int ny = cy + dir.dy; // calculate the new y coordinate
+            if (between(nx, x) && between(ny, y) // if the new coordinates are within the maze
+                    && (maze[nx][ny] == 0)) { // and the new coordinates are not visited
+                maze[cx][cy] |= dir.bit; // mark the current cell as visited
+                maze[nx][ny] |= dir.opposite.bit; // mark the new cell as visited
+                generateMaze(nx, ny); // generate the maze from the new cell
             }
         }
     }
 
     private static boolean between(int v, int upper) {
         return (v >= 0) && (v < upper);
-    }
+    }  // check if the new coordinates are within the maze and not visited (used for the recursive backtracking algorithm)
 
-    private enum DIR {
+    private enum DIR { // initializing the directions
         N(1, 0, -1), S(2, 0, 1), E(4, 1, 0), W(8, -1, 0);
         private final int bit;
         private final int dx;
@@ -66,14 +66,14 @@ public class MazeGenerator {
         private DIR opposite;
 
         // use the static initializer to resolve forward references
-        static {
+        static { // initialise the opposite direction for each direction
             N.opposite = S;
             S.opposite = N;
             E.opposite = W;
             W.opposite = E;
         }
 
-        DIR(int bit, int dx, int dy) {
+        DIR(int bit, int dx, int dy) { // constructor of the direction
             this.bit = bit;
             this.dx = dx;
             this.dy = dy;
